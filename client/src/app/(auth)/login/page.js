89ed8a773f4309ec730f908bf47c1d2a245522b5
aaditya paudel <adityaspaudel@gmail.com>
2025-10-28 14:13:08 +0545

    css update in login page

--- client/src/app/(auth)/login/page.js
+++ client/src/app/(auth)/login/page.js
@@ -22,72 +22,80 @@ const LoginForm = () => {
 	const [apiMessage, setApiMessage] = useState(null);
 	const router = useRouter();
 	return (
-		<div className="flex flex-col justify-center items-center">
-			<h1>Login</h1>
-
-			<Formik
-				initialValues={formData}
-				validationSchema={loginSchema}
-				onSubmit={async (values) => {
-					await sleep(500);
-					alert(JSON.stringify(values, null, 2));
-					try {
-						const response = await fetch(
-							`http://localhost:8000/user/userLogin`,
-							{
-								method: "POST",
-								headers: { "Content-Type": "application/json" },
-								body: JSON.stringify(values),
-							}
-						);
-						const data = await response.json();
-						setFormData(values);
-
-						console.log(data);
-						setApiMessage(data.message);
-
-						if (!response.ok) throw new Error("could not login");
-						if (data.role == "admin") router.push(`/admin/${data.id}/home`);
-						else if (data.role == "guest")
-							router.push(`/guest/${data.id}/home`);
-						else if (data.role == "staff")
-							router.push(`/staff/${data.id}/home`);
-					} catch (error) {
-						console.error("login failed", error);
-						setApiMessage(error.message);
-					} finally {
-						setTimeout(() => {
-							setApiMessage(null);
-						}, [5000]);
-					}
-				}}
-			>
-				{({ isSubmitting }) => (
-					<Form className="flex flex-col">
-						<label htmlFor="email">Email</label>
-						<Field name="email" placeholder="jane@acme.com" type="email" />
-						<label htmlFor="password">Password</label>
-						<Field
-							name="password"
-							placeholder="jane@acme.com"
-							type="password"
-						/>
-
-						<button type="submit" disabled={isSubmitting}>
-							Submit
-						</button>
-					</Form>
-				)}
-			</Formik>
-			{apiMessage && (
-				<div>
-					{apiMessage == "login successful" ? (
-						<div className="text-green-400">{apiMessage}</div>
-					) : (
-						<div className="text-red-400"></div>
+		<div className="flex flex-col justify-center items-center bg-gray-100 h-screen w-screen text-black">
+			<div className="flex flex-col gap-2 p-8 justify-center items-center  border-gray-600 h-1/2 w-1/3 rounded-sm bg-fuchsia-200 shadow hover:shadow-md hover:shadow-black transition ">
+				<h1 className="font-bold">Login</h1>
+				<Formik
+					initialValues={formData}
+					validationSchema={loginSchema}
+					onSubmit={async (values) => {
+						await sleep(500);
+						alert(JSON.stringify(values, null, 2));
+						try {
+							const response = await fetch(
+								`http://localhost:8000/user/userLogin`,
+								{
+									method: "POST",
+									headers: { "Content-Type": "application/json" },
+									body: JSON.stringify(values),
+								}
+							);
+							const data = await response.json();
+							setFormData(values);
+							console.log(data);
+							setApiMessage(data.message);
+							if (!response.ok) throw new Error("could not login");
+							if (data.role == "admin") router.push(`/admin/${data.id}/home`);
+							else if (data.role == "guest")
+								router.push(`/guest/${data.id}/home`);
+							else if (data.role == "staff")
+								router.push(`/staff/${data.id}/home`);
+						} catch (error) {
+							console.error("login failed", error);
+							setApiMessage(error.message);
+						} finally {
+							setTimeout(() => {
+								setApiMessage(null);
+							}, [5000]);
+						}
+					}}
+				>
+					{({ isSubmitting }) => (
+						<Form className="flex flex-col  text-sm">
+							<label htmlFor="email">Email</label>
+							<Field
+								className="px-2 border rounded-sm bg-white"
+								name="email"
+								placeholder="jane@acme.com"
+								type="email"
+							/>
+							<label htmlFor="password">Password</label>
+							<Field
+								className="px-2 border rounded-sm bg-white"
+								name="password"
+								placeholder="pa$$word"
+								type="password"
+							/>
+							<button
+								className="mt-8 text text-gray-50 bg-pink-500 transition hover:bg-pink-600"
+								type="submit"
+								disabled={isSubmitting}
+							>
+								login
+							</button>
+						</Form>
 					)}
-				</div>
-			)}
+				</Formik>
+				{apiMessage && (
+					<div>
+						{apiMessage == "login successful" ? (
+							<div className="text-green-400">{apiMessage}</div>
+						) : (
+							<div className="text-red-400"></div>
+						)}
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
