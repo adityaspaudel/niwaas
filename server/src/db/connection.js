272b58f95babede4fa3714431ba8f2e31c8fb5d5
aaditya paudel <adityaspaudel@gmail.com>
2025-11-24 22:30:39 +0545

    switched from pnpm to npm, mongodblocal to remote

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 
 const dotenv = require("dotenv");
 dotenv.config();
-
+// const MONGODB_ATLAS_URI="mongodb+srv://adityaspaudel_db_user:LEC54C55C3UuyQay@cluster0.wt2hkw2.mongodb.net/?appName=Cluster0";
 const dbConnect = async () => {
-	try {
-		const isConnected = await mongoose.connect(process.env.MONGODB_URL);
-		if (!isConnected) {
-			console.error("failed to connect mongodb");
-		}
-		console.log("connected to mongodb");
-	} catch (error) {
-		console.error("database connection failed", error);
-	}
+  try {
+    const isConnected = await mongoose.connect(process.env.MONGODB_ATLAS_URI);
+    if (!isConnected) {
+      console.error("failed to connect mongodb");
+    }
+    console.log(`connected to mongodb ${process.env.MONGODB_ATLAS_URI}`);
+  } catch (error) {
+    console.error("database connection failed", error);
+  }
 };
 
 module.exports = { dbConnect };
